298a60b64682bfad17e9c85a7c5e883f3236f491
Mohmad012 <mohmadailsia@gmail.com>
2022-08-28 01:12:39 +0200

    add base url for home page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { DarkModeContextProvider } from "./context/darkMode";
 // basename="template_react"
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="AdminDashboard">
     <DarkModeContextProvider>
       <App />
     </DarkModeContextProvider>
